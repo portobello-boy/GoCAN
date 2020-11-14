9c58123a3b01dd606916232aebf5304572af4501
TheBiggerFish <clhnumber4@gmail.com>
2020-11-14 11:55:59 -0500

    Added comments

--- data/data.go
+++ data/data.go
@@ -5,16 +5,17 @@ import (
 	"net/http"
 )
 
+// ParseData handles transforming http.Request into DataRequest with error handling
 func ParseData(w http.ResponseWriter, r *http.Request) DataRequest {
 	var dataReq DataRequest
 	err := json.NewDecoder(r.Body).Decode(&dataReq)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-
 	return dataReq
 }
 
+// ParseJoin handles transforming http.Request into JoinRequest with error handling
 func ParseJoin(w http.ResponseWriter, r *http.Request) JoinRequest {
 	var joinReq JoinRequest
 	err := json.NewDecoder(r.Body).Decode(&joinReq)
